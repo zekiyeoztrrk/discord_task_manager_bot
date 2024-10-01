321195e2a8b0a318e80e37aa76c06a9521a5ca62
zekiyeoztrrk <63965783+zekiyeoztrrk@users.noreply.github.com>
2024-10-01 23:08:27 +0300

    Update test_add_task.py

--- tests/test_add_task.py
+++ tests/test_add_task.py
@@ -1,12 +1,12 @@
 import unittest
 from database import add_task, show_tasks
 
-class TestTaskManager(unittest.TestCase):
-
+class TestAddTask(unittest.TestCase):
     def test_add_task(self):
-        add_task("Test Task")
+        task_description = "Test task"
+        add_task(task_description)
         tasks = show_tasks()
-        self.assertTrue(any(task[1] == "Test Task" for task in tasks))
+        self.assertIn(task_description, [task[1] for task in tasks])
 
 if __name__ == '__main__':
     unittest.main()
